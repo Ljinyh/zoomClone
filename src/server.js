0d98e2cb13a2a693bd83b4af55d08a26e977d833
jinhui_LEE <105095093+Ljinyh@users.noreply.github.com>
2022-11-22 17:41:52 +0900

    add/ redirect 추가

--- src/server.js
+++ src/server.js
@@ -14,6 +14,9 @@ app.use('/public', express.static(__dirname + '/public'));
 // '/views/home.pug 렌더 시키기
 app.get('/', (req, res) => res.render('home'));
 
+// 어떤 Url을 써도 home "/" 으로 돌아가게 만듦 (다른 url 을 쓰지 않을 예정)
+app.get('/*', (req, res) => res.redirect('/'));
+
 const PORT = 3000;
 const handleLiten = () => console.log(`Listening on http://localhost:${PORT}`)
 app.listen(PORT, handleLiten);
